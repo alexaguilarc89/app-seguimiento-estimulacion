07e18b0e4c7b304383e0092baf3e877c81c25edd
Alex AC <alexaguilarc89@email.com>
2026-06-26 15:52:43 -0500

    Initial commit - Spring boot project app-seguimiento

---
+++ src/main/java/com/alex/appseguimiento/AppSeguimientoEstimulacionApplication.java
@@ -0,0 +1,13 @@
+package com.alex.appseguimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSeguimientoEstimulacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSeguimientoEstimulacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/appseguimiento/AppSeguimientoEstimulacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.appseguimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSeguimientoEstimulacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
